4e9e985720b1e1b10f092d504d78e13f63f5b344
Max Ageev <ageev.makson@yandex.ru>
2022-07-12 14:43:11 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+
+        char[] vowels = {'a','e','o','i','u','y'};
+        char[] consonant ={'q','w','r','t','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'};
+
+        for(char c :vowels){
+            if(c == character){
+                System.out.println("Vowel");
+            }else {
+                for(char b : consonant){
+                    if(b == character){
+                        System.out.println("Consonant");
+                    }
+                    else {
+                        System.out.println("wrong alphabet");
+                    }
+                }
+            }
+
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -3,5 +3,18 @@ package school.mjc.stage0.conditions.task3;
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
 
+    if(month <=12) {
+        if (month <= 5 && month > 2) {
+            System.out.println("Spring");
+        } else if (month <= 8 && month > 5)
+            System.out.println("Summer");
+        else if (month <= 11 && month > 8) {
+            System.out.println("Autumn");
+        } else {
+            System.out.println("Winter");
+        }
+    }else {
+        System.out.println("wrong number!");
+    }
     }
 }
