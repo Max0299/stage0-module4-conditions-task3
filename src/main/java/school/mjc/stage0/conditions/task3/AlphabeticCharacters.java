e4c9902ab1934864e787f1ba17778dda468f414e
Max Ageev <ageev.makson@yandex.ru>
2022-07-12 19:21:50 +0300

    all done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -3,25 +3,15 @@ package school.mjc.stage0.conditions.task3;
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
 
-        char[] vowels = {'a','e','o','i','u','y'};
         char[] consonant ={'q','w','r','t','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'};
 
-        for(char c :vowels){
-            if(c == character){
-                System.out.println("Vowel");
-                break;
-            }else {
-                for(char b : consonant){
-                    if(b == character){
-                        System.out.println("Consonant");
-                        break;
-                    }
-                    else {
-                        System.out.println("wrong alphabet");
-                    }
-                }
-            }
-
+        if( character == 'a'|| character == 'e' || character =='o' || character == 'i' || character =='u'|| character == 'y'){
+            System.out.println("Vowel");
+        }else if(character == 'q'|| character == 'w' || character =='r' || character == 't' || character =='p'|| character == 's' || character == 'd'|| character == 'f' || character =='g' || character == 'h' || character =='j'|| character == 'k' || character == 'l'|| character == 'z' || character =='x' || character == 'c' || character =='v'|| character == 'b'|| character =='n'|| character == 'm'){
+            System.out.println("Consonant");
+        }else {
+            System.out.println("wrong alphabet!");
         }
+
     }
 }
